25e3288b8a4e0bbb9878ccc5ca34f497bad1b9d1
Lawrence Siebert <gryftir@gmail.com>
2014-05-02 16:17:35 -0700

    print title

--- scrape_owbn.pl
+++ scrape_owbn.pl
@@ -19,6 +19,8 @@ foreach my $link (@links) {
 		 my($rel_link, $element, $attr, $tag) = @$game;
 		 $mech->get($start_url . $rel_link);
 		 my $game_tree = HTML::TreeBuilder->new_from_content($mech->content());
+		 my $title =  $game_tree->look_down(_tag => 'title')->as_text() . "\n";
+		 print $title, "-" x length($title), "\n";
 		 foreach my $p_tags ($game_tree->look_down(_tag => 'p') ) {
 			 my $string = $p_tags->as_text();
 			 if ($string =~ /(ST Email List|Head Storyteller)/i) {
